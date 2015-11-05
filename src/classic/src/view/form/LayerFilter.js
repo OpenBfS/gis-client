6ed84462157d517d776a13fe5fa0f22c2f9c12a0
Marc Jansen <jansen@terrestris.de>
2015-11-05 17:46:55 +0100

    Ongoing filter fine tuning

--- src/classic/src/view/form/LayerFilter.js
+++ src/classic/src/view/form/LayerFilter.js
@@ -378,6 +378,12 @@ Ext.define("Koala.view.form.LayerFilter", {
     },
 
     createValueFilter: function(filter, idx) {
+        var allowedValues = null;
+        var isCombo = false
+        if (filter.allowedValues) {
+            allowedValues = filter.allowedValues.split(",");
+            isCombo = true;
+        }
         var fieldSet = Ext.create('Ext.form.FieldSet', {
             padding: 5,
             defaults: {anchor: '100%'},
@@ -391,7 +397,7 @@ Ext.define("Koala.view.form.LayerFilter", {
                     xtype: 'textfield',
                     labelWidth: 70,
                     name: filter.param,
-                    fieldLabel: filter.param,
+                    fieldLabel: filter.alias,
                     value: filter.value,
                     emptyText: filter.defaultValue
                 }
