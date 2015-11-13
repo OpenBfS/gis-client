87fd7ca68449b88056930c508b80631f9b8f44c5
Marc Jansen <jansen@terrestris.de>
2015-11-13 23:39:17 +0100

    Enable test coverage

---
+++ src/test/phantom_hooks.js
@@ -0,0 +1,17 @@
+/*eslint-env node*/
+/*eslint no-console:0*/
+module.exports = {
+  afterEnd: function(runner) {
+    var fs = require('fs');
+    var coverage = runner.page.evaluate(function() {
+      return window.__coverage__;
+    });
+
+    if (coverage) {
+      console.log('Writing coverage to coverage/coverage.json');
+      fs.write('coverage/coverage.json', JSON.stringify(coverage), 'w');
+    } else {
+      console.log('No coverage data generated');
+    }
+  }
+};
