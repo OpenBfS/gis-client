b2ae84dbb93fe19075f9cfb01dd011cd4a11b74a
Marc Jansen <jansen@terrestris.de>
2016-03-04 10:00:23 +0100

    Add TODO with regard to unicode and btoa

--- src/app/store/MetadataSearch.js
+++ src/app/store/MetadataSearch.js
@@ -48,6 +48,8 @@ Ext.define('Koala.store.MetadataSearch', {
 
         if(this.proxy.username && this.proxy.password){
             var tok = this.proxy.username + ':' + this.proxy.password;
+            // TODO we may want to use something UTF-8 safe:
+            // https://developer.mozilla.org/de/docs/Web/API/WindowBase64/btoa#Unicode-Zeichenketten
             var hash = btoa(tok);
             this.proxy.headers = {};
             this.proxy.headers.Authorization = "Basic " + hash;
