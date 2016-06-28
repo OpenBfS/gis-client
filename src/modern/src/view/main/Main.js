3ca988daa0cf04ee0e2f98d563f65b839fd0c030
Kai Volland <kaivolland@yahoo.de>
2016-06-28 10:39:13 +0200

    Fix basic layout.

--- src/modern/src/view/main/Main.js
+++ src/modern/src/view/main/Main.js
@@ -1,5 +1,5 @@
 Ext.define('Koala.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Panel',
     xtype: 'app-main',
 
     requires: [
@@ -18,105 +18,57 @@ Ext.define('Koala.view.main.Main', {
         'Koala.view.container.MobileMenu'
     ],
 
-    fullscreen: true,
-    tabBarPosition: 'bottom',
-
     defaults: {
         styleHtmlContent: true
     },
 
-    items: [
-        {
-            title: 'Map',
-            name: 'mapcontainer',
-            iconCls: 'x-fa fa-map-marker',
-            layout: 'fit',
+    layout: {
+        type: 'card'
+    },
+
+    items: [{
+        title: 'Map',
+        name: 'mapcontainer',
+        // iconCls: 'x-fa fa-map-marker',
+        items: [{
+            xtype: 'basigx-component-map',
+            appContextPath: 'resources/appContext.json',
+            height: '100%'
+        }, {
+            xtype: 'container',
+            layout: 'vbox',
+            style: {
+                top: '20px',
+                right: '20px',
+                position: 'absolute'
+            },
+            defaults: {
+                margin: '0 0 5 0',
+                scale: 'large'
+            },
             items: [{
-                xtype: 'basigx-component-map',
-                appContextPath: 'resources/appContext.json',
-                height: '300px'
-
-            },
-            {
-                xtype: 'toolbar',
-                vertical: true,
-                width: 50,
-                cls: 'basigx-map-tools',
-                x: 0,
-                y: 0,
-                style: {
-                    top: '20px',
-                    position: 'absolute'
-                },
-                defaults: {
-                    scale: 'large'
-                },
-                items: [
-                    {
-                        xtype: 'basigx-button-zoomin'
-                    }, {
-                        xtype: 'basigx-button-zoomout'
-                    }, {
-                        xtype: 'basigx-button-zoomtoextent'
-                    }
-                ]
-            },
-            {
-                xtype: 'toolbar',
-                vertical: true,
-                width: 50,
-                cls: 'koala-hamburger-toolbar',
-                x: 0,
-                y: 0,
-                style: {
-                    top: '20px',
-                    position: 'absolute',
-                    left: '20px'
-                },
-                defaults: {
-                    scale: 'large'
-                },
-                items: [
-                    {
-                        xtype: 'k-button-hamburger'
-                    }
-                ]
-            },
-            {
-                xtype: 'toolbar',
-                vertical: true,
-                width: 50,
-                cls: 'koala-mobilelegend-toolbar',
-                x: 0,
-                y: 0,
-                style: {
-                    bottom: '20px',
-                    position: 'absolute',
-                    right: '0'
-                },
-                defaults: {
-                    scale: 'large'
-                },
-                items: [
-                    {
-                        xtype: 'k-button-mobileaddlayer'
-                    },
-                    {
-                        xtype: 'k-button-mobileshowlegend'
-                    }
-                ]
+                xtype: 'basigx-button-zoomin'
+            }, {
+                xtype: 'basigx-button-zoomout'
+            }, {
+                xtype: 'basigx-button-zoomtoextent'
             }]
-        },
-        {
-            xtype: 'k-container-mobileaddlayer'
-        },
-        {
-            xtype: 'k-container-mobilelegend'
-        },
-        {
-            xtype: 'k-container-mobilemenu',
-            hidden: true
-        }
-
-    ]
+        }, {
+            xtype: 'k-button-hamburger',
+            style: {
+                top: '20px',
+                position: 'absolute',
+                left: '20px'
+            }
+        }, {
+            xtype: 'k-button-mobileshowlegend',
+            style: {
+                bottom: '20px',
+                position: 'absolute',
+                right: '20px'
+            }
+        }]
+    }, {
+        xtype: 'k-container-mobileaddlayer'
+    }]
 });
