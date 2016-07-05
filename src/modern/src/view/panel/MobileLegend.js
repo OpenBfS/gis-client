7864ffc22eb5f6ac8409f249d99a315f55997ff4
Daniel Koch <koch@terrestris.de>
2016-07-05 16:06:05 +0200

    WIP: Modern legend component

--- src/modern/src/view/panel/MobileLegend.js
+++ src/modern/src/view/panel/MobileLegend.js
@@ -1,3 +1,21 @@
+/* Copyright (c) 2015-2016 terrestris GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @class Koala.view.panel.MobileLegend
+ */
 Ext.define('Koala.view.panel.MobileLegend',{
     extend: 'Koala.view.panel.MobilePanel',
     xtype: 'k-panel-mobilelegend',
@@ -9,10 +27,15 @@ Ext.define('Koala.view.panel.MobileLegend',{
 
     controller: 'k-panel-mobilelegend',
     viewModel: {
-        type: 'k-panel-mobilelegend'
+        type: 'k-panel-themetree'
     },
 
-    config: {
-        title: 'Legend'
+    bind: {
+        title: '{title}'
+    },
+
+    listeners: {
+        initialize: 'onInitialize',
+        show: 'onShow'
     }
 });
--- src/modern/src/view/panel/MobileLegendController.js
+++ src/modern/src/view/panel/MobileLegendController.js
@@ -1,5 +1,134 @@
 Ext.define('Koala.view.panel.MobileLegendController', {
     extend: 'Ext.app.ViewController',
-    alias: 'controller.k-panel-mobilelegend'
+    alias: 'controller.k-panel-mobilelegend',
+
+    /**
+     *
+     */
+    onInitialize: function() {
+        var me = this;
+        me.createTreeList();
+    },
+
+    onShow: function() {
+        var me = this;
+        me.setInitialCheckStatus();
+    },
+
+    /**
+     *
+     */
+    createTreeList: function() {
+        var me = this;
+        var view = me.getView();
+        var treeStore;
+        var treePanel;
+
+        treeStore = Ext.create('GeoExt.data.store.LayersTree', {
+            layerGroup: Ext.ComponentQuery.query('basigx-component-map')[0].getMap().getLayerGroup(),
+            filters: [
+                function(rec) {
+                    var layer = rec.getOlLayer();
+                    var showKey = 'bp_displayInLayerSwitcher';
+                    if (layer.get(showKey) === false) {
+                        return false;
+                    }
+                    return true;
+                }
+            ]
+        });
+
+        treePanel = Ext.create('Ext.list.Tree', {
+            store: treeStore,
+            listeners: {
+                tap: {
+                    fn: me.onTreeItemTap,
+                    element: 'element'
+                },
+                scope: me
+            }
+        });
+
+        view.add(treePanel);
+    },
+
+    setInitialCheckStatus: function() {
+        var me = this;
+        var view = me.getView();
+        var treeList = view.down('treelist');
+        var treeListItems = treeList.itemMap;
+
+        Ext.iterate(treeListItems, function(k, item) {
+            if (item instanceof Ext.list.TreeItem) {
+                me.setTreeItemCheckStatus(item);
+            }
+        });
+
+    },
+
+    setTreeItemCheckStatus: function(item) {
+        var me = this;
+        var layer = item.getNode().getOlLayer();
+        item.setText(me.getTreeListItemTpl().apply(layer));
+    },
+
+    onTreeItemTap: function(evt, target) {
+        var me = this;
+        var view = me.getView();
+        var treeList = view.down('treelist');
+        var selection = treeList.getSelection();
+        var layer = selection ? selection.getOlLayer() : null;
+
+        if (layer && layer instanceof ol.layer.Layer) {
+            layer.setVisible(!layer.getVisible());
+            me.setTreeItemCheckStatus(treeList.getItem(selection));
+        }
+    },
+
+    getTreeListItemTpl: function() {
+        var me = this;
+
+        return new Ext.XTemplate(
+            '<tpl if="this.isVisible(values)">',
+                '<i class="fa fa-eye"></i> {text} <br> <span style="color:#666;">{[this.getFilterText(values)]}</span>',
+                '<tpl if="this.isRemovable(values)">',
+                    '<i class="fa fa-times"></i>',
+                '</tpl>',
+            '<tpl else>',
+                '<i class="fa fa-eye-slash"></i> {text}',
+            '</tpl>', {
+                isVisible: function(layer) {
+                    return layer.getVisible();
+                },
+                getFilterText: function(layer) {
+                    return Koala.util.Layer.getFiltersTextFromMetadata(layer.metadata);
+                },
+                isRemovable: function(layer) {
+                    return layer.get('allowRemoval') || false;
+                }
+            }
+        );
+    },
+
+    removalHandler: function(btn){
+        var layer = btn.layerRec.getOlLayer();
+        var map = Ext.ComponentQuery.query('basigx-component-map')[0]
+            .getMap();
+
+        Ext.Msg.show({
+            title: 'Info',
+            message: 'Layer <b>' + layer.get('name') +
+                '</b> aus Karte entfernen?',
+            buttonText: {
+                yes: "Ja",
+                no: "Nein"
+            },
+            fn: function(btnId){
+                if(btnId === "yes"){
+                    map.removeLayer(layer);
+                }
+            }
+        });
+    },
 
 });
