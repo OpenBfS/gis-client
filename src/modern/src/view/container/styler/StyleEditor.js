ab5c7946745f50b2971a9d0b9e8e97890614f8a7
Marc Jansen <jansen@terrestris.de>
2016-10-14 12:42:17 +0200

    Minimum fixes for making the modern build pass
    This adds two placeholder views to the modern sources which are required by some
    files in the app folder. I have added TODO annotations to double check these
    later.
    
    Also changed is the app.json, so that the requirement for BasiGX.util.Color
    can be satisfied.

---
+++ src/modern/src/view/container/styler/StyleEditor.js
@@ -0,0 +1,11 @@
+/**
+ * This class only exists to satisfy the compiler.
+ *
+ * TODO we need to check where in `app` we require this view and probably change
+ *      this.
+ *
+ * @class Koala.view.container.styler.Rules
+ */
+Ext.define("Koala.view.container.styler.Rules", {
+    extend: "Ext.Component"
+});
