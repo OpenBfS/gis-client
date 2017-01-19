653741f270033d9398e39752d35f194238ad9801
Kai Volland <kaivolland@yahoo.de>
2017-01-19 11:25:31 +0100

    Restructuring and documentation.

--- src/app/Application.js
+++ src/app/Application.js
@@ -27,7 +27,6 @@ Ext.define('Koala.Application', {
 
     name: 'Koala',
 
-
     statics: {
         /**
          * Return the current timereference for the application or null if
@@ -59,6 +58,23 @@ Ext.define('Koala.Application', {
         }
     },
 
+    routes: {
+        'map/:lon/:lat/:zoom': {
+            action: 'onMapRoute'
+        },
+        'layers/:layers': {
+            action: 'onLayerTreeRoute',
+            before: 'beforeLayerTreeRoute',
+            conditions: {
+                ':layers': "(.*)"
+            }
+        }
+    },
+
+    stores: [
+        // TODO: add global / shared stores here
+    ],
+
     listen: {
         controller: {
             '#': {
@@ -67,18 +83,59 @@ Ext.define('Koala.Application', {
         }
     },
 
+    /**
+     * Handles an unmachted routing hashstring;
+     * @param  {String} hash The hashstring.
+     */
     onUnmatchedRoute: function(hash) {
         Ext.log.info('Unmatched route: ', hash);
     },
 
     /**
      * A look-up object that will be filled in #beforeLayerTreeRoute so that
-     * the actual #onLayerTreeRoute can do its work. Cosnsist of uuids as keys
+     * the actual #onLayerTreeRoute can do its work. Consist of uuids as keys
      * and actual OpenLayers layers as values.
      *
      * @private
      */
     routeCreatedLayers: null,
+
+    /**
+     * Stores the permalinkFilters of the permalink after parsing it.
+     *
+     * @private
+     */
+    permalinkFilters: {},
+
+    /**
+     * Listens to the 'map/:lon/:lat/:zoom' route and sets the map to the
+     * specified values.
+     * @param {Integer} lon Longituted of the mapView center.
+     * @param {Integer} lat Latitued of the mapView center.
+     * @param {Integer} zoom Zoom of the mapView.
+     */
+    onMapRoute: function(lon, lat, zoom) {
+        var view = this.getMainView();
+        var map = view.down('basigx-component-map').getMap();
+        var mapView = map.getView();
+
+        mapView.setCenter([lon, lat]);
+        mapView.setZoom(zoom);
+    },
+
+    /**
+     * [onAppUpdate description]
+     * @return {[type]} [description]
+     */
+    onAppUpdate: function () {
+        Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
+            function (choice) {
+                if (choice === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    },
 
     /**
      * Called before the actual #onLayerTreeRoute handlers layer adding to the
@@ -87,9 +144,9 @@ Ext.define('Koala.Application', {
      * property #routeCreatedLayers, and the routing action is only then
      * resumed when all layers have been build.
      *
-     * @param String layers The has part that triggered the route and
+     * @param {String} layers The has part that triggered the route and
      *     that looks like 'uuid:state,otheruuid:otherstate'
-     * @param Object action A routing action element
+     * @param {Object} action A routing action element
      * @private
      */
     beforeLayerTreeRoute: function(layers, action){
@@ -99,104 +156,19 @@ Ext.define('Koala.Application', {
         var gotLayers = 0;
         var routeCreatedLayers = {};
         var LayerUtil = Koala.util.Layer;
-        var permalinkFilters = {};
 
         Ext.each(layerUuidsWithStates, function(uuidWithState) {
             var uuid = uuidWithState.split("_")[0];
-            var filters = uuidWithState.split("_")[2];
-            permalinkFilters[uuid] = {};
-
-            if (filters) {
-                var filtersArray = filters.split(";");
-                Ext.each(filtersArray, function(filter){
-                    var filterParts = filter.split('{');
-                    var prefix = filterParts[0];
-                    var filterValues = filterParts[1].replace('}', '');
-
-                    switch (prefix) {
-                        case 'pt':
-                            var t = parseInt(filterValues.split('t')[1], 10);
-                            if (Ext.isNumber(t)) {
-                                permalinkFilters[uuid].pointintime = {
-                                    timeinstant: new Date(t)
-                                };
-                            }
-                        break;
-                        case 'tr':
-                            var s = parseInt(filterValues.split('s')[1], 10);
-                            var e = parseInt(filterValues.split('e')[1], 10);
-                            if (Ext.isNumber(s) && Ext.isNumber(s)) {
-                                permalinkFilters[uuid].timerange = {
-                                    mindatetimeinstant: new Date(s),
-                                    maxdatetimeinstant: new Date(e)
-                                };
-                            }
-                        break;
-                        case 'at':
-                            var alias = filterValues.split("=")[0];
-                            var val = filterValues.split("=")[1];
-                            permalinkFilters[uuid].value = {
-                                alias: alias,
-                                value: val
-                            };
-                        break;
-                        case 'st':
-                            permalinkFilters[uuid].Stil = {
-                                alias: 'Stil',
-                                value: filterValues
-                            };
-                        break;
-                        default:
-                        break;
-                    }
-                });
-            }
+            var filtersString = uuidWithState.split("_")[2];
 
             if (Koala.util.String.isUuid(uuid)) {
                 expectedLayers++;
+                me.parseFiltersFromPermalink(uuid, filtersString);
                 LayerUtil.getMetadataFromUuidAndThen(uuid, function(md){
                     gotLayers++;
                     var metadataClone = Ext.clone(md);
 
-                    // Apply permalink-values to filter
-                    Ext.each(md.filters, function(mdFilter){
-                        var permalinkFilter = permalinkFilters[uuid][mdFilter.alias] || permalinkFilters[uuid][mdFilter.type];
-                        var minDate;
-                        var maxDate;
-                        if(permalinkFilter){
-                            if (mdFilter.type === "pointintime") {
-                                maxDate = Ext.Date.parse(mdFilter.maxdatetimeinstant, mdFilter.maxdatetimeformat);
-                                minDate = Ext.Date.parse(mdFilter.mindatetimeinstant, mdFilter.mindatetimeformat);
-                                if(minDate < permalinkFilter.timeinstant && maxDate > permalinkFilter.timeinstant){
-                                    Ext.apply(mdFilter, permalinkFilter);
-                                }
-                            }
-                            if (mdFilter.type === "timerange") {
-                                maxDate = Ext.Date.parse(mdFilter.maxdatetimeinstant, mdFilter.maxdatetimeformat);
-                                minDate = Ext.Date.parse(mdFilter.mindatetimeinstant, mdFilter.mindatetimeformat);
-                                if(minDate < permalinkFilter.mindatetimeinstant && maxDate > permalinkFilter.mindatetimeinstant &&
-                                    minDate < permalinkFilter.maxdatetimeinstant && maxDate > permalinkFilter.maxdatetimeinstant){
-                                    Ext.apply(mdFilter, permalinkFilter);
-                                }
-                            }
-                            // if (mdFilter.type === "value" && !(mdFilter.alias === "Stil")) {
-                            //     var allowedStore = Koala.util.Filter.getStoreFromAllowedValues(mdFilter.allowedValues);
-                            //     debugger
-                            //     Ext.each(mdFilter.allowedValues.split(","), function(a){
-                            //         if(a === permalinkFilter.value) {
-                            //             Ext.apply(mdFilter, permalinkFilter);
-                            //         }
-                            //     });
-                            // }
-                            if (mdFilter.type === "value") {
-                                var allowedStore = Koala.util.Filter.getStoreFromAllowedValues(mdFilter.allowedValues);
-                                var matchingRecord = allowedStore.findRecord('dsp', permalinkFilter.value);
-                                if(matchingRecord){
-                                    Ext.apply(mdFilter, permalinkFilter);
-                                }
-                            }
-                        }
-                    });
+                    me.applyPermalinkFiltersToMetadata(uuid, md.filters);
 
                     var olLayer = LayerUtil.layerFromMetadata(md);
                     LayerUtil.setOriginalMetadata(olLayer, metadataClone);
@@ -208,6 +180,126 @@ Ext.define('Koala.Application', {
                 });
            } else {
                Ext.log.info('Skipping route part ', uuidWithState);
+            }
+        });
+    },
+
+    /**
+     * Parses the String representation of a filter an stores the filters in
+     * me.permalinkFilters.
+     *
+     * @param {String} uuid The metadataidentifier comming from gnos.
+     * @param {String} filtersString The string representation of filtersString.
+     *     pointintime: "pt{t[timestamp]}"
+     *     e.g.: Wed Jan 18 2017 15:10:14 GMT+0100 (CET)
+     *         "pt{t1484748614084}"
+     *
+     *     timerange: "tr{s[starttimestamp]e[endtimestamp]}"
+     *     e.g. From Mon Jun 01 2015 02:00:00 GMT+0200 (CEST)
+     *          Till Tue Jun 30 2015 02:00:00 GMT+0200 (CEST):
+     *         "tr{s1433116800000e1435622400000}"
+     *
+     *     attribute: "at{[filteralias]=[value]}"
+     *     e.g.: Messnetz = KFÜ
+     *          "at{Messnetz='triangle'}"
+     *
+     *     style: "st{[stylename]}"
+     *     e.g.: Style should be 'blue-point'
+     *          "st{point}"
+     * @private
+     */
+    parseFiltersFromPermalink: function(uuid, filtersString){
+        var me = this;
+        if (filtersString) {
+            me.permalinkFilters[uuid] = {};
+
+            var filtersArray = filtersString.split(";");
+            Ext.each(filtersArray, function(filter){
+                var filterParts = filter.split('{');
+                var prefix = filterParts[0];
+                var filterValues = filterParts[1].replace('}', '');
+
+                switch (prefix) {
+                    case 'pt':
+                        var t = parseInt(filterValues.split('t')[1], 10);
+                        if (Ext.isNumber(t)) {
+                            me.permalinkFilters[uuid].pointintime = {
+                                timeinstant: new Date(t)
+                            };
+                        }
+                    break;
+                    case 'tr':
+                        var s = parseInt(filterValues.split('s')[1], 10);
+                        var e = parseInt(filterValues.split('e')[1], 10);
+                        if (Ext.isNumber(s) && Ext.isNumber(s)) {
+                            me.permalinkFilters[uuid].timerange = {
+                                mindatetimeinstant: new Date(s),
+                                maxdatetimeinstant: new Date(e)
+                            };
+                        }
+                    break;
+                    case 'at':
+                        //TODO Check for special 'test_data'
+                        var alias = filterValues.split("=")[0];
+                        var val = filterValues.split("=")[1];
+                        me.permalinkFilters[uuid].value = {
+                            alias: alias,
+                            value: val
+                        };
+                    break;
+                    case 'st':
+                        me.permalinkFilters[uuid].Stil = {
+                            alias: 'Stil',
+                            value: filterValues
+                        };
+                    break;
+                    default:
+                        Ext.log.warn('Could not parase filter from permalink ' +
+                            filter);
+                    break;
+                }
+            });
+        }
+    },
+
+    /**
+     * Applies the filters stored in me.permalinkFilters to the metadata of a
+     * layer. parseFiltersFromPermalink should be executed before.
+     * @param {String} uuid The metadataidentifier of the layer to apply the
+     *                      filters to.
+     * @param {Array} metadataFilters An array of objects representing a
+     *                                metadataFilter.
+     * @private
+     */
+    applyPermalinkFiltersToMetadata: function(uuid, metadataFilters) {
+        var me = this;
+        Ext.each(metadataFilters, function(mdFilter) {
+            var permalinkFilter = me.permalinkFilters[uuid][mdFilter.alias] || me.permalinkFilters[uuid][mdFilter.type];
+            var minDate;
+            var maxDate;
+            if(permalinkFilter){
+                if (mdFilter.type === "pointintime") {
+                    maxDate = Ext.Date.parse(mdFilter.maxdatetimeinstant, mdFilter.maxdatetimeformat);
+                    minDate = Ext.Date.parse(mdFilter.mindatetimeinstant, mdFilter.mindatetimeformat);
+                    if(minDate < permalinkFilter.timeinstant && maxDate > permalinkFilter.timeinstant){
+                        Ext.apply(mdFilter, permalinkFilter);
+                    }
+                }
+                if (mdFilter.type === "timerange") {
+                    maxDate = Ext.Date.parse(mdFilter.maxdatetimeinstant, mdFilter.maxdatetimeformat);
+                    minDate = Ext.Date.parse(mdFilter.mindatetimeinstant, mdFilter.mindatetimeformat);
+                    if(minDate < permalinkFilter.mindatetimeinstant && maxDate > permalinkFilter.mindatetimeinstant &&
+                        minDate < permalinkFilter.maxdatetimeinstant && maxDate > permalinkFilter.maxdatetimeinstant){
+                        Ext.apply(mdFilter, permalinkFilter);
+                    }
+                }
+                if (mdFilter.type === "value") {
+                    var allowedStore = Koala.util.Filter.getStoreFromAllowedValues(mdFilter.allowedValues);
+                    var matchingRecord = allowedStore.findRecord('val', permalinkFilter.value);
+                    if(matchingRecord){
+                        Ext.apply(mdFilter, permalinkFilter);
+                    }
+                }
             }
         });
     },
@@ -239,46 +331,8 @@ Ext.define('Koala.Application', {
             }
         });
         me.routeCreatedLayers = null;
-    },
-
-    /**
-     *
-     */
-    onMapRoute: function(lon, lat, zoom) {
-        var view = this.getMainView();
-        var map = view.down('basigx-component-map').getMap();
-        var mapView = map.getView();
-
-        mapView.setCenter([lon, lat]);
-        mapView.setZoom(zoom);
-    },
-
-    routes: {
-        'map/:lon/:lat/:zoom': {
-            action: 'onMapRoute'
-        },
-        'layers/:layers': {
-            action: 'onLayerTreeRoute',
-            before: 'beforeLayerTreeRoute',
-            conditions: {
-                ':layers': "(.*)"
-            }
-        }
-    },
-
-    stores: [
-        // TODO: add global / shared stores here
-    ],
-
-    onAppUpdate: function () {
-        Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
-            function (choice) {
-                if (choice === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
     }
+
 }, function() {
     // Remove the annyoing warning '[W] targetCls is missing. This may mean that
     // getTargetEl() …' see: https://www.sencha.com/forum/showthread.php?288898-
