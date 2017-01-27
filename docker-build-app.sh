5b302ad69fcbfdbeb76dee49e216d98dc5fda111
Marco Lechner, Bundesamt fuer Strahlenschutz, Freiburg, Germany <mlechner@bfs.de>
2017-01-27 16:49:39 +0100

    updated docker-buid-app.sh script (to be used with ./Dockerfile)

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -3,54 +3,22 @@
 CUR_DIR=`pwd`
 WORKSPACE=$CUR_DIR
 SCRIPT_DIR=`dirname $0`
-SENCHA_CMD_VERSION="6.2.1"
-SENCHA_CMD_MINOR_VERSION="6.2.1.29"
-SENCHA_WS="/sencha-ws"
-BASIGX_HASH="daf2442"
+SENCHA_CMD_VERSION="6.2.1.29"
+EXTJS_VERSION="6.2.0"
 
 cd $SCRIPT_DIR
 wget http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
 unzip SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
-./SenchaCmd-$SENCHA_CMD_MINOR_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_MINOR_VERSION"
+./SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_VERSION"
+wget http://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip
+unzip ext-$EXTJS_VERSION-gpl.zip
 
-SENCHA_CMD="/opt/$SENCHA_CMD_MINOR_VERSION/sencha"
-CONFIG="gis_client_configs"
-
-$SENCHA_CMD -sdk $WORKSPACE/src/ext generate workspace $SENCHA_WS
-$SENCHA_CMD package repo init -name "terrestris GmbH & Co. KG" -email "info@terrestris.de"
-$SENCHA_CMD package repo add GeoExt http://geoext.github.io/geoext3/cmd/pkgs
-cd $SENCHA_WS/packages/
-git clone https://github.com/terrestris/BasiGX.git
-cd BasiGX/
-git checkout $BASIGX_HASH
-$SENCHA_CMD package upgrade
-$SENCHA_CMD package list
-$SENCHA_CMD package build
-
+SENCHA_CMD="/opt/$SENCHA_CMD_VERSION/sencha"
 
 cd $WORKSPACE/src/
-$SENCHA_CMD package list
-$SENCHA_CMD package remove GeoExt
-$SENCHA_CMD package remove BasiGX
-$SENCHA_CMD package list
-$SENCHA_CMD package repo add GeoExt http://geoext.github.io/geoext3/cmd/pkgs
-$SENCHA_CMD package add $SENCHA_WS/build/BasiGX/BasiGX.pkg
-$SENCHA_CMD package list
 
-if [ -f $WORKSPACE/$CONFIG/appContext.json ]; then
-    cp $WORKSPACE/$CONFIG/appContext.json $WORKSPACE/src/resources/
-fi
-
-if [ -f $WORKSPACE/$CONFIG/layerprofile.json ]; then
-    cp $WORKSPACE/$CONFIG/layerprofile.json $WORKSPACE/src/classic/resources/
-fi
-
-if [ -f $WORKSPACE/$CONFIG/layerset.json ]; then
-    cp $WORKSPACE/$CONFIG/layerset.json $WORKSPACE/src/classic/resources/
-fi
-
+$SENCHA_CMD app upgrade $SCRIPT_DIR/ext-$EXTJS_VERSION
 $SENCHA_CMD app clean
-$SENCHA_CMD app upgrade
 $SENCHA_CMD app build
 
 ln -sf $WORKSPACE/src/build/production/Koala $WORKSPACE/webgis
