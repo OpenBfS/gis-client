a49db15ef7712b60c274e97eda881200d239ac2f
Kai Volland <kaivolland@yahoo.de>
2017-03-03 13:42:23 +0100

    Prepare basic testsetup for MobileLegendController.

---
+++ src/test/spec/modern/panel/MobileLegendController.test.js
@@ -0,0 +1,10 @@
+Ext.Loader.syncRequire(['Koala.view.panel.MobileLegendController']);
+
+describe('Koala.view.panel.MobileLegendController', function() {
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(Koala.view.panel.MobileLegendController).to.not.be(undefined);
+        });
+    });
+
+});
