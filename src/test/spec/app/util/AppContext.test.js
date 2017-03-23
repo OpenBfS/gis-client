567baf2636952b4293103cadc746ed77f320eb84
M. Jansen <jansen@terrestris.de>
2017-03-23 14:26:33 +0100

    Add different tests for the bfs project

---
+++ src/test/spec/app/util/AppContext.test.js
@@ -0,0 +1,59 @@
+Ext.Loader.syncRequire([
+    'Koala.util.AppContext',
+    'BasiGX.view.component.Map'
+]);
+
+describe('Koala.util.AppContext', function() {
+
+    afterEach(function() {
+        delete Koala.Application;
+    });
+
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(Koala.util.AppContext).to.not.be(undefined);
+        });
+    });
+
+    describe('Static functions', function() {
+
+        describe('#getAppContext', function() {
+            // var mapComponent;
+            it('is defined', function() {
+                expect(Koala.util.AppContext.getAppContext).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(Koala.util.AppContext.getAppContext).to.be.a(Function);
+            });
+        });
+
+        describe('#generateCheckToolVisibility', function() {
+            it('is defined', function() {
+                expect(Koala.util.AppContext.generateCheckToolVisibility)
+                    .to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(Koala.util.AppContext.generateCheckToolVisibility)
+                    .to.be.a(Function);
+            });
+        });
+
+        describe('#hasTool', function() {
+            it('is defined', function() {
+                expect(Koala.util.AppContext.hasTool).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(Koala.util.AppContext.hasTool).to.be.a(Function);
+            });
+        });
+
+        describe('#getMergedDataByKey', function() {
+            it('is defined', function() {
+                expect(Koala.util.AppContext.getMergedDataByKey).to.not.be(undefined);
+            });
+            it('is a function', function() {
+                expect(Koala.util.AppContext.getMergedDataByKey).to.be.a(Function);
+            });
+        });
+    });
+});
