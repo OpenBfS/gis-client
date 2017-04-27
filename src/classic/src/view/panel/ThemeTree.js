ee8b63f4587f26dd60476d215c0804d9a24d236e
Kai Volland <kaivolland@yahoo.de>
2017-04-27 10:36:37 +0200

    Update tree on expand to react to layerset changes.

--- src/classic/src/view/panel/ThemeTree.js
+++ src/classic/src/view/panel/ThemeTree.js
@@ -76,7 +76,10 @@ Ext.define('Koala.view.panel.ThemeTree', {
 
     listeners: {
         select: 'setupShowFilterWinCheck',
-        itemdblclick: 'addLayerWithDefaultFilters'
+        itemdblclick: 'addLayerWithDefaultFilters',
+        expand: function() {
+            this.update();
+        }
     },
 
     initComponent: function() {
