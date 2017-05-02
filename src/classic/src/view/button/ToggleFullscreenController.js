421af656895423978f5e6d9e1ecdbd75481d03fb
Kai Volland <kaivolland@yahoo.de>
2017-05-02 17:00:35 +0200

    Introduces document listener for fullscreen button glyph.

--- src/classic/src/view/button/ToggleFullscreenController.js
+++ src/classic/src/view/button/ToggleFullscreenController.js
@@ -27,17 +27,8 @@ Ext.define('Koala.view.button.toggleFullscreenController', {
     /**
      *
      */
-    onClick: function(btn) {
+    onToggle: function() {
         Koala.util.Fullscreen.toggleFullscreen();
-/* 
-        var btnClass = Koala.util.Fullscreen.isInFullscreen() ?
-                'fa-compress' : 'fa-expand';
-        btn.setHtml('<i class="fa ' + btnClass + ' fa-2x"></i>');
-*/
-        var glyphCode = Koala.util.Fullscreen.isInFullscreen() ?
-                'f065' : 'f066';
-        btn.setGlyph('x' + glyphCode + '@FontAwesome');
-       
     }
 
 });
