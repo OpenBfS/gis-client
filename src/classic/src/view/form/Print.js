e823ec99f66988ebcd44261ffcd170126d3dac0c
Andreas Schmitz <schmitz@terrestris.de>
2017-06-12 12:09:30 +0200

    also print carto window lines

--- src/classic/src/view/form/Print.js
+++ src/classic/src/view/form/Print.js
@@ -580,6 +580,8 @@ Ext.define('Koala.view.form.Print', {
                             return false;
                         }
                         printLayers.push(layer);
+                    } else if (layer.get('proofPrintable')) {
+                        printLayers.push(layer);
                     } else {
                         return false;
                     }
