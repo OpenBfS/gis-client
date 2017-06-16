1f7f47247cebccc0e1f92f813643ecfb86f78fea
Kai Volland <kaivolland@yahoo.de>
2017-06-16 16:15:15 +0200

    Prepare coverage.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -50,7 +50,6 @@ module.exports = function(config) {
             'test/spec/' + TOOLKIT + '/**/*.test.js'
         ],
 
-
         // list of files to exclude
         exclude: [
             // './resources/lib/**/test/*.js'
@@ -58,31 +57,28 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
-
+        preprocessors: {
+            'app/**/*.js': ['coverage'],
+            'classic/**/*.js': ['coverage']
+        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'mocha'],
-
+        reporters: ['progress', 'mocha', 'coverage'],
 
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
@@ -105,6 +101,12 @@ module.exports = function(config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+
+        // optionally, configure the reporter
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        }
     });
 };
