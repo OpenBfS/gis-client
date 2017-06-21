19645c7500d11b2b6dee0ce44ad617633d7e826c
Andreas Schmitz <schmitz@terrestris.de>
2017-06-21 14:29:12 +0200

    added more mapcontroller tests

--- src/test/spec/app/view/component/MapController.test.js
+++ src/test/spec/app/view/component/MapController.test.js
@@ -1,11 +1,36 @@
 Ext.Loader.syncRequire([
-    'Koala.view.component.MapController'
+    'Koala.view.component.MapController',
+    'BasiGX.view.component.Map'
 ]);
 
 describe('Koala.view.component.MapController', function() {
+
     describe('Basics', function() {
         it('is defined', function() {
             expect(Koala.view.component.MapController).to.not.be(undefined);
         });
+
+        it('can be created', function() {
+            var controller = new Koala.view.component.MapController();
+            expect(controller).to.not.be(undefined);
+        });
+
+        it('can be called', function() {
+            var controller = new Koala.view.component.MapController();
+            var view = {};
+            view.getMap = sinon.stub();
+            var feat = {};
+            var layer = {};
+            layer.getProperties = sinon.stub().returns({});
+            layer.get = sinon.stub().returns({});
+            feat.get = sinon.stub()
+                .returns({})
+                .withArgs('layer')
+                .returns(layer);
+
+            controller.setView(view);
+            controller.onHoverFeatureClick([feat]);
+        });
     });
+
 });
