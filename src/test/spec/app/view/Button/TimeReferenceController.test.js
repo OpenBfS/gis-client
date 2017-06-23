e2d362cf1274a867bbeed2c264eb455a648f8a3f
Andreas Schmitz <schmitz@terrestris.de>
2017-06-23 14:52:21 +0200

    completed button controller tests

--- src/test/spec/app/view/Button/TimeReferenceController.test.js
+++ src/test/spec/app/view/Button/TimeReferenceController.test.js
@@ -7,5 +7,17 @@ describe('Koala.view.button.TimeReferenceController', function() {
         it('is defined', function() {
             expect(Koala.view.button.TimeReferenceController).to.not.be(undefined);
         });
+
+        it('binds translated texts', function() {
+            var ctrl = new Koala.view.button.TimeReferenceController();
+            Koala.view.button.TimeReference = {UTC: 'utc'};
+            var btn = {};
+            btn.setBind = sinon.stub();
+            btn.blur = sinon.stub();
+            btn.getCurrent = sinon.stub();
+            ctrl.setTextBinds(btn);
+            expect(btn.setBind.calledOnce).to.be(true);
+            expect(btn.blur.calledOnce).to.be(true);
+        });
     });
 });
