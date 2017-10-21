60cb800f08fca561a08f560e4409668efba626b4
Marco Lechner, GeoBoink <marco@marcolechner.de>
2017-10-21 18:26:54 +0200

    this commit seems to repair docker build

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -6,8 +6,6 @@ WORKSPACE=$CUR_DIR
 SENCHA_CMD_VERSION="6.2.2.36"
 EXTJS_VERSION="6.2.0"
 
-ls -hal && ls -hal src/lib/*
-
 if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip ]
 then
   wget http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
@@ -27,13 +25,11 @@ SENCHA_CMD="/opt/$SENCHA_CMD_VERSION/sencha"
 
 cd $WORKSPACE/src/
 
-$SENCHA_CMD app upgrade $WORKSPACE/ext-$EXTJS_VERSION
+ln -s ${WORKSPACE}/ext-${EXTJS_VERSION} ext
 $SENCHA_CMD app install --framework=ext
 $SENCHA_CMD app clean
 $SENCHA_CMD app build
 
 ln -sf $WORKSPACE/src/build/production/Koala $WORKSPACE/webgis
-
-#$SENCHA_CMD app watch
 
 cd $WORKSPACE
