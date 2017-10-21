ccec2c1657b2e9d4250a363f1a8501cc4405556b
Marco Lechner, GeoBoink <marco@marcolechner.de>
2017-10-21 19:01:45 +0200

    minor changes in docker-build-app

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -2,34 +2,34 @@
 
 CUR_DIR=`pwd`
 WORKSPACE=$CUR_DIR
-#SENCHA_CMD_VERSION="6.5.2.15"
 SENCHA_CMD_VERSION="6.2.2.36"
 EXTJS_VERSION="6.2.0"
 
-if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip ]
+if [ ! -e SenchaCmd-${SENCHA_CMD_VERSION}-linux-amd64.sh.zip ]
 then
-  wget http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+  wget http://cdn.sencha.com/cmd/${SENCHA_CMD_VERSION}/no-jre/SenchaCmd-${SENCHA_CMD_VERSION}-linux-amd64.sh.zip
 fi
 
-unzip SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
-./SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_VERSION"
+unzip SenchaCmd-${SENCHA_CMD_VERSION}-linux-amd64.sh.zip
+./SenchaCmd-${SENCHA_CMD_VERSION}-linux-amd64.sh -q -dir "/opt/${SENCHA_CMD_VERSION}"
 
-if [ ! -e ext-$EXTJS_VERSION-gpl.zip ]
+if [ ! -e ext-${EXTJS_VERSION}-gpl.zip ]
 then
-  wget http://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip
+  wget http://cdn.sencha.com/ext/gpl/ext-${EXTJS_VERSION}-gpl.zip
 fi
 
-unzip ext-$EXTJS_VERSION-gpl.zip
+unzip ext-${EXTJS_VERSION}-gpl.zip
 
-SENCHA_CMD="/opt/$SENCHA_CMD_VERSION/sencha"
+SENCHA_CMD="/opt/${SENCHA_CMD_VERSION}/sencha"
 
-cd $WORKSPACE/src/
+cd ${WORKSPACE}/src/
 
 ln -s ${WORKSPACE}/ext-${EXTJS_VERSION} ext
-$SENCHA_CMD app install --framework=ext
-$SENCHA_CMD app clean
-$SENCHA_CMD app build
+${SENCHA_CMD} app install --framework=ext
+#${SENCHA_CMD} app upgrade ${WORKSPACE}/ext-${EXTJS_VERSION}
+${SENCHA_CMD} app clean
+${SENCHA_CMD} app build
 
-ln -sf $WORKSPACE/src/build/production/Koala $WORKSPACE/gis
+ln -sf ${WORKSPACE}/src/build/production/Koala ${WORKSPACE}/gis
 
-cd $WORKSPACE
+cd ${WORKSPACE}
