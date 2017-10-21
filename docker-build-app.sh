89ff6e474fc12f4dff97d8b2414c2b615ce96b0b
Marco Lechner, GeoBoink <marco@marcolechner.de>
2017-10-21 18:03:26 +0200

    making docker build handle existing Sencha and ext archives

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -6,18 +6,29 @@ WORKSPACE=$CUR_DIR
 SENCHA_CMD_VERSION="6.2.2.36"
 EXTJS_VERSION="6.2.0"
 
-wget http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+ls -hal && ls -hal src/lib/*
+
+if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip ]
+then
+  wget http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+fi
+
 unzip SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
 ./SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_VERSION"
-wget http://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip
+
+if [ ! -e ext-$EXTJS_VERSION-gpl.zip ]
+then
+  wget http://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip
+fi
+
 unzip ext-$EXTJS_VERSION-gpl.zip
 
 SENCHA_CMD="/opt/$SENCHA_CMD_VERSION/sencha"
 
 cd $WORKSPACE/src/
 
-$SENCHA_CMD app install --framework=ext
 $SENCHA_CMD app upgrade $WORKSPACE/ext-$EXTJS_VERSION
+$SENCHA_CMD app install --framework=ext
 $SENCHA_CMD app clean
 $SENCHA_CMD app build
 
