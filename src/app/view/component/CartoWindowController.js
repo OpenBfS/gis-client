27588d2d73abca1ee85d13d1e4134bc8df128dfd
Andreas Schmitz <schmitz@terrestris.de>
2018-03-29 11:55:20 +0200

    prevent disabling of all interactions

--- src/app/view/component/CartoWindowController.js
+++ src/app/view/component/CartoWindowController.js
@@ -141,9 +141,13 @@ Ext.define('Koala.view.component.CartoWindowController', {
      *
      */
     disableMapInteractions: function() {
+        if (this.disabled) {
+            return;
+        }
         var map = this.getView().getMap();
         var me = this;
         this.interactionActiveList = [];
+        this.disabled = true;
         map.getInteractions().forEach(function(interaction) {
             me.interactionActiveList.push(interaction.getActive());
             interaction.setActive(false);
@@ -167,6 +171,7 @@ Ext.define('Koala.view.component.CartoWindowController', {
             var active = me.interactionActiveList ? me.interactionActiveList[idx] : true;
             interaction.setActive(active);
         });
+        this.disabled = false;
     },
 
     /**
