66cbba697fe4d22b9eccf330636870e8062df355
Johannes Weskamm <weskamm@terrestris.de>
2018-04-18 11:19:43 +0200

    refresh legendgraphic on filterchange, fixes #1972

--- src/classic/src/view/panel/RoutingLegendTree.js
+++ src/classic/src/view/panel/RoutingLegendTree.js
@@ -1054,10 +1054,18 @@ Ext.define('Koala.view.panel.RoutingLegendTree', {
         var store = this.getStore();
         store.each(function(rec) {
             var layer = rec.getOlLayer();
-            var selector = '[name=' + layer.get('routeId') + '-legendImg]';
-            var img = Ext.ComponentQuery.query(selector)[0];
-            if (img && img.el && img.el.dom) {
-                img.setSrc(Koala.util.Layer.getCurrentLegendUrl(layer));
+            var legend = Ext.ComponentQuery.query(
+                'k-panel-routing-legendtree')[0];
+            var view = legend.getView();
+            var node = view.getNode(rec);
+            if (node && node.querySelector &&
+                node.querySelector('img')) {
+                var selector = node.querySelector('img').id;
+                var img = Ext.ComponentQuery.query(
+                    '[id=' + selector + ']')[0];
+                if (img && img.el && img.el.dom) {
+                    img.setSrc(Koala.util.Layer.getCurrentLegendUrl(layer));
+                }
             }
         });
     },
