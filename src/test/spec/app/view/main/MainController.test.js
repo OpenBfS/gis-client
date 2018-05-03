b020e5393788bcb81a8116696d99732b02cb2aba
Andreas Schmitz <schmitz@terrestris.de>
2018-05-03 16:01:13 +0200

    reverted addition of test which cannot be run without a patch

--- src/test/spec/app/view/main/MainController.test.js
+++ src/test/spec/app/view/main/MainController.test.js
@@ -1,16 +1,11 @@
 Ext.Loader.syncRequire([
-    'Koala.view.main.MainController',
-    'Koala.view.panel.ThemeTree'
+    'Koala.view.main.MainController'
 ]);
 
 describe('Koala.view.main.MainController', function() {
     describe('Basics', function() {
         it('is defined', function() {
             expect(Koala.view.main.MainController).to.not.be(undefined);
-        });
-        it('can be created', function() {
-            var main = Ext.create('Koala.view.main.Main');
-            expect(main).to.not.be(undefined);
         });
     });
 });
