5fcf368eeb4223061c1940d0803c3b59bcf7571d
Andreas Schmitz <schmitz@terrestris.de>
2018-05-03 15:54:08 +0200

    added test to create the main component

--- src/test/spec/app/view/main/MainController.test.js
+++ src/test/spec/app/view/main/MainController.test.js
@@ -1,11 +1,16 @@
 Ext.Loader.syncRequire([
-    'Koala.view.main.MainController'
+    'Koala.view.main.MainController',
+    'Koala.view.panel.ThemeTree'
 ]);
 
 describe('Koala.view.main.MainController', function() {
     describe('Basics', function() {
         it('is defined', function() {
             expect(Koala.view.main.MainController).to.not.be(undefined);
+        });
+        it('can be created', function() {
+            var main = Ext.create('Koala.view.main.Main');
+            expect(main).to.not.be(undefined);
         });
     });
 });
