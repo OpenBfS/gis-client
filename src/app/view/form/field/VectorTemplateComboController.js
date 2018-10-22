0138ea9697326a3fcf7097f4b949386033e8d075
Andreas Schmitz <schmitz@terrestris.de>
2018-10-22 10:59:32 +0200

    use safer query to check for allowClone

--- src/app/view/form/field/VectorTemplateComboController.js
+++ src/app/view/form/field/VectorTemplateComboController.js
@@ -21,6 +21,7 @@ Ext.define('Koala.view.form.field.VectorTemplateComboController', {
     alias: 'controller.k-form-field-vectortemplatecombo',
 
     requires: [
+        'Koala.util.Object'
     ],
 
     /**
@@ -32,9 +33,10 @@ Ext.define('Koala.view.form.field.VectorTemplateComboController', {
         var appContext = Ext.ComponentQuery.query('k-component-map')[0].appContext;
         var map = Ext.ComponentQuery.query('k-component-map')[0].map;
         var templates = appContext.data.merge.vectorTemplates.slice();
+        var path = 'metadata/layerConfig/olProperties/allowClone';
         if (this.getView().getIncludeCloneLayers()) {
             Ext.each(map.getLayers().getArray(), function(layer) {
-                if (layer.metadata && layer.metadata.layerConfig.olProperties.allowClone) {
+                if (layer.metadata && Koala.util.Object.getPathStrOr(layer, path)) {
                     templates.push({
                         uuid: layer.metadata.id,
                         label: layer.metadata.treeTitle
