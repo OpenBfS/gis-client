31762fb46c9af299d571b062b0556f001dd8534b
Andreas Schmitz <schmitz@terrestris.de>
2019-04-03 12:43:51 +0200

    Add modern fake variant of the chart settings menu

---
+++ src/modern/src/view/menu/ChartSettingsMenu.js
@@ -0,0 +1,10 @@
+/**
+ * This class only exists to satisfy the compiler.
+ *
+ * Needed due to the way the carto window works in classic.
+ *
+ * @class Koala.view.menu.ChartSettingsMenu
+ */
+Ext.define('Koala.view.menu.ChartSettingsMenu', {
+    extend: 'Ext.Component'
+});
