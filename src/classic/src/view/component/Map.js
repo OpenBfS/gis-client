30b367ea9dfbebc8982754ddc5fab2ebd810dae3
Jakob Miksch <jakob@meggsimum.de>
2021-07-06 14:05:10 +0200

    Cleanup proj4 definitions

--- src/classic/src/view/component/Map.js
+++ src/classic/src/view/component/Map.js
@@ -73,11 +73,8 @@ Ext.define('Koala.view.component.Map', {
             hoverPlugin.highlightStyleFunction = highlightStyleFunction;
         }
 
-        // setup all the projections required for the application
-        proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs');
-        proj4.defs('EPSG:3857', '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs');
-        proj4.defs('EPSG:25832','+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
-
+        // register projections defined in
+        // "src/resources/lib/proj4js/proj4-defs.js"
         ol.proj.proj4.register(proj4);
 
         // TODO We should move this to another place.
